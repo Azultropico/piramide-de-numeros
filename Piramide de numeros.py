d5d6dec32ac01680d2b2cac82c14f6e96959e83b
Azultropico <90474697+Azultropico@users.noreply.github.com>
2021-09-15 09:43:40 -0300

    Add files via upload

---
+++ Piramide de numeros.py
@@ -0,0 +1,12 @@
+def imprimir(valor):
+    if isinstance(valor, int):
+        x = 1
+        while x <= valor:
+            y = 1
+            texto = " "
+            while y <= x:
+                texto += str(y) + "\t"
+                y +=1
+            print(texto)
+            x +=1
+imprimir(5)
